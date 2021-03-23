a1c1bd8491a48eeb34398463236b8c7692ae7418
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2021-03-23 18:47:40 +0000

    Merging previous networking attempts scripts

---
+++ Assets/Scripts/UI/Menu/UIHostGameMenu.cs
@@ -0,0 +1,18 @@
+using Game.Managers;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIHostGameMenu : MonoBehaviour
+{
+    public void HostGame()
+    {
+        var newNetworkDiscovery = GameObject.FindObjectOfType<NewNetworkDiscovery>();
+        MySceneManager.LoadScene("GameView");
+        Mirror.NetworkManager.singleton.StartHost();
+        newNetworkDiscovery.AdvertiseServer();
+
+    }
+}
